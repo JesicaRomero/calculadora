de7c0a3598e2ba649783f8d9842a20f498c7d1c2
Jessica Romero <jessica@MacBook-Air-de-Jessica.local>
2022-10-26 13:26:14 +0200

    Copiado reto 2 dia 5

---
+++ index.js
@@ -0,0 +1,9 @@
+let sum = (op1, op2 = 0) => op1 + op2
+
+let subs = (op1, op2 = 0) => op1 - op2
+
+let mult = (op1, op2 = 0) => op1 * op2
+
+let div = (op1, op2 = 0) => op1 / op2
+
+module.exports = { sum, subs, mult, div }
---
+++ run.js
@@ -0,0 +1,6 @@
+let myLib = require('./index')
+
+console.log(myLib.sum(20))
+console.log(myLib.subs(20, 5))
+console.log(myLib.div(20, 5))
+console.log(myLib.mult(20, 5))
