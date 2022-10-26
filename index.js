732e3029835c3197e21e3ffa0f50fa8897139685
Jessica Romero <jessica@MacBook-Air-de-Jessica.local>
2022-10-26 13:30:24 +0200

    añadida funcion cuadrado

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ let mult = (op1, op2 = 0) => op1 * op2
 
 let div = (op1, op2 = 0) => op1 / op2
 
-module.exports = { sum, subs, mult, div }
+let square = (op1) => op1 * op1
+
+module.exports = { sum, subs, mult, div, square }
